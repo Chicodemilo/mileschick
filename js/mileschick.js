1000b147c370745ae03490fdcc465c9d3aca2e4f
Chicodemilo <mileschick@gmail.com>
2016-12-14 17:07:20 -0600

    SVGs and background movement

--- js/mileschick.js
+++ js/mileschick.js
@@ -10,14 +10,4 @@ $(document).ready(function() {
           $('#miles_image').css("background-position", newvalueX+"px     "+newvalueY+"px");
 	});
 
-	// var movementStrength = 25;
-	// var height = movementStrength / $(window).height();
-	// var width = movementStrength / $(window).width();
-	// $("#star_image").mousemove(function(e){
- //          var pageX = e.pageX - ($(window).width() / 2);
- //          var pageY = e.pageY - ($(window).height() / 2);
- //          var newvalueX = width * pageX * -1 - 25;
- //          var newvalueY = height * pageY * -1 - 50;
- //          $('#star_image').css("background-position", newvalueX+"px     "+newvalueY+"px");
-	// });
 });
